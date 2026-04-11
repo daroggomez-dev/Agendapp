0ffc78d0ee7220cfb7f4c1219198484a12414ccd
daroggomez-dev <daro.g.gomez@gmail.com>
2026-04-11 05:09:56 +0000

    Fix Firebase persistence error

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,6 +1,6 @@
 import{initializeApp}from'firebase/app';
 import{getAuth}from'firebase/auth';
-import{getFirestore,enableIndexedDbPersistence}from'firebase/firestore';
+import{getFirestore}from'firebase/firestore';
 const firebaseConfig={
 apiKey:"AIzaSyCThYvmbWeVhYJK9FGhPmJtpmJNFRiTiGI",
 authDomain:"agendapp-4c4e9.firebaseapp.com",
@@ -12,5 +12,4 @@ appId:"1:580440694741:web:6049807a6752b27c68a75c"
 const app=initializeApp(firebaseConfig);
 export const auth=getAuth(app);
 export const db=getFirestore(app);
-enableIndexedDbPersistence(db).catch(()=>{});
 export default app;
